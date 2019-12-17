63c99d975934ba0f3ba6014a271e2a9e41f0380b
Kunihiro Ishiguro <ishi@hash-set.com>
2019-12-17 01:38:42 -0800

    Update to tokio 0.2 APIs.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
-use std::time::Duration;
-use tokio::runtime::Runtime;
-//use futures::join;
+use futures::future::join_all;
+use tokio::time::Duration;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let rt = Runtime::new()?;
+#[tokio::main]
+async fn main() -> Result<(), failure::Error> {
     println!("- start({:?})", std::thread::current().id());
 
+    let mut handles = Vec::new();
+
     for i in 0..40 {
-        rt.spawn(async move {
+        handles.push(tokio::spawn(async move {
             println!("start({:?}) {}", std::thread::current().id(), i);
             tokio::time::delay_for(Duration::from_secs(i)).await;
             println!("end({:?}) {}", std::thread::current().id(), i);
-        });
+        }));
     }
     println!("---- non blocked routine ----");
 
-    //rt.shutdown_on_idle();
+    join_all(handles).await;
+
     println!("- end({:?})", std::thread::current().id());
 
-    loop {}
     Ok(())
 }
