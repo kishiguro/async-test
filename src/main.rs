2c5166bd4b88ead05c212bd271d4925f5419cb16
Kunihiro Ishiguro <ishi@hash-set.com>
2020-02-15 04:18:12 -0800

    Use join_all.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
+use futures::future::join_all;
 use std::time::Duration;
-use tokio::runtime::Runtime;
-//use futures::join;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let rt = Runtime::new()?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("- start({:?})", std::thread::current().id());
 
+    let mut handles = Vec::new();
     for i in 0..40 {
-        rt.spawn(async move {
+        let h = tokio::spawn(async move {
             println!("start({:?}) {}", std::thread::current().id(), i);
             tokio::time::delay_for(Duration::from_secs(i)).await;
             println!("end({:?}) {}", std::thread::current().id(), i);
         });
+        handles.push(h)
     }
     println!("---- non blocked routine ----");
-
-    //rt.shutdown_on_idle();
     println!("- end({:?})", std::thread::current().id());
 
-    loop {}
+    join_all(handles).await;
+
     Ok(())
 }
