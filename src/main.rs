84d57c9808d537e2c8a9acb2649ed2ec3959831f
Kunihiro Ishiguro <ishi@hash-set.com>
2020-02-15 04:30:50 -0800

    non tokio_main.  create runtime by manually.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 use futures::future::join_all;
+use tokio::runtime::Runtime;
 use tokio::time::Duration;
 
-#[tokio::main]
-async fn main() -> Result<(), failure::Error> {
-    println!("- start({:?})", std::thread::current().id());
+fn main() -> Result<(), failure::Error> {
+    let mut rt = Runtime::new()?;
 
-    let mut handles = Vec::new();
+    rt.block_on(async {
+        let mut handles = Vec::new();
 
-    for i in 0..40 {
-        handles.push(tokio::spawn(async move {
-            println!("start({:?}) {}", std::thread::current().id(), i);
-            tokio::time::delay_for(Duration::from_secs(i)).await;
-            println!("end({:?}) {}", std::thread::current().id(), i);
-        }));
-    }
-    println!("---- non blocked routine ----");
+        for i in 0..40 {
+            handles.push(tokio::spawn(async move {
+                println!("start({:?}) {}", std::thread::current().id(), i);
+                tokio::time::delay_for(Duration::from_secs(i)).await;
+                println!("end({:?}) {}", std::thread::current().id(), i);
+            }));
+        }
+        println!("---- non blocked routine ----");
 
-    join_all(handles).await;
-
-    println!("- end({:?})", std::thread::current().id());
-
+        join_all(handles).await;
+    });
     Ok(())
 }
